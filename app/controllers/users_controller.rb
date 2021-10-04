6d5c03c7a108f8caff2630e5ca1dde12c5f9fba0
Victor Prado <victorpradoestudo@gmail.com>
2021-10-04 18:50:16 -0300

    routes(criação dos resources), user(criação de validações e hash), user_controller(definição de actions), new.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    # definição das actions:
+
+
+    # renderiza o formulário 
+    def new
+        @user = User.new
+    end
+
+
+    # recebe um post pra fazer a criação da rota
+    def create
+    end
+
 end
