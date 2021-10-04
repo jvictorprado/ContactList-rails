d9cd4a22dcbfacee87f90fab643b9edd652e9743
Victor Prado <victorpradoestudo@gmail.com>
2021-10-04 19:35:34 -0300

    new.html(render), index.html(new_user_path), applicatio), header.html, users_controller(definição create e user_params)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,17 @@ class UsersController < ApplicationController
 
     # recebe um post pra fazer a criação da rota
     def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = 'Usuário cadastrado com sucesso'
+            redirect_to root_url
+        else
+            render 'new'
+        end
     end
 
+    private 
+        def user_params 
+            params.require(:user).permit(:email, :name, :password, :password_confirmation)
+        end
 end
