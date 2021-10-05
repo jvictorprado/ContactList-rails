58d0c52767498153d41e416e544adb51d75b45e0
Victor Prado <victorpradoestudo@gmail.com>
2021-10-04 23:04:55 -0300

    Autenticação do Usuário(sessions)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    # todos os controllers que herdam de ApplicationController terão os modulos do SessionsHelper
+    include SessionsHelper
+
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  
+  # previnir que o usuário já logado não "entre de novo" na aplicação
+  def new
+    redirect_to user_path(current_user) if user_signed_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in(user)
+      # redirecionando
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Email ou senha inválidos'
+      render 'new'  
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = 'Logout com sucesso'
+    redirect_to entrar_path
+  end
+
+end
